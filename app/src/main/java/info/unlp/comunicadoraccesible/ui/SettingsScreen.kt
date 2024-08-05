592fb1bf274b67dc9fcfbeea26ebe9f0b9525873
Thomas Rusiecki <rusieckithomas@gmail.com>
2024-08-05 19:19:32 -0300

    settings screen reworked

--- app/src/main/java/info/unlp/comunicadoraccesible/ui/SettingsScreen.kt
+++ app/src/main/java/info/unlp/comunicadoraccesible/ui/SettingsScreen.kt
@@ -1,184 +1,160 @@
 package info.unlp.comunicadoraccesible.ui
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.Card
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import info.unlp.comunicadoraccesible.composables.CustomSlider
 import info.unlp.comunicadoraccesible.composables.ScalableText
 import info.unlp.comunicadoraccesible.data.AccessibilityViewModel
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(viewModel: AccessibilityViewModel, navController: NavController) {
-    val context = LocalContext.current
+    var checkBoxSetting by remember { mutableStateOf(false) }
+    var textValue by remember { mutableFloatStateOf(1f) }
+    var uiValue by remember { mutableFloatStateOf(1f) }
+    var ttsPitch by remember { mutableFloatStateOf(1f) }
+    var ttsSpeed by remember { mutableFloatStateOf(1f) }
 
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
+    Scaffold(
+        modifier = Modifier.background(MaterialTheme.colorScheme.background),
+        topBar = {
+            TopAppBar(title = { Text("Opciones") })
+        }
+    ) { padding ->
         Column(
             modifier = Modifier
-                .weight(1f)
-                .padding(8.dp)
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Card(
-                modifier = Modifier.fillMaxWidth(),
-            ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    ScalableText(
-                        text = "Tamaño del texto",
-                        textStyle = MaterialTheme.typography.bodyLarge,
-                        accessibilityViewModel = viewModel
-                    )
-                    Box(
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-                        CustomSlider(
-                            label = "Tamaño del texto",
-                            viewModel = viewModel,
-                            value = viewModel.textScale,
-                            onValueChange = {
-                                viewModel.updateTextScale(it)
-                            },
-                            valueRange = 1f..1.5f,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-                }
+            SettingCheckbox("Modo Alto Contraste", checkBoxSetting) {
+                checkBoxSetting = it
             }
 
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Card(
-                modifier = Modifier.fillMaxWidth(),
-            ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    ScalableText(
-                        text = "Tamaño de la interfaz",
-                        textStyle = MaterialTheme.typography.bodyLarge,
-                        accessibilityViewModel = viewModel
-                    )
-                    Box(
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-                        CustomSlider(
-                            label = "Tamaño de la Interfaz",
-                            viewModel = viewModel,
-                            value = viewModel.buttonSize,
-                            onValueChange = { viewModel.updateButtonSize(it) },
-                            valueRange = 1f..1.5f,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-                }
-            }
-        }
-
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(8.dp)
-        ) {
-            Card(
-                modifier = Modifier.fillMaxWidth(),
-            ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    ScalableText(
-                        text = "Pitch del Texto a Voz",
-                        textStyle = MaterialTheme.typography.bodyLarge,
-                        accessibilityViewModel = viewModel
-                    )
-                    Box(
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-                        CustomSlider(
-                            label = "Pitch",
-                            viewModel = viewModel,
-                            value = viewModel.ttsPitch,
-                            onValueChange = { viewModel.updatePitch(it) },
-                            valueRange = -0.0f..2f,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-                }
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Card(
-                modifier = Modifier.fillMaxWidth(),
-            ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    ScalableText(
-                        text = "Velocidad",
-                        textStyle = MaterialTheme.typography.bodyLarge,
-                        accessibilityViewModel = viewModel
-                    )
-                    Box(
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-                        CustomSlider(
-                            label = "Velocidad del Texto a Voz",
-                            viewModel = viewModel,
-                            value = viewModel.ttsSpeed,
-                            onValueChange = { viewModel.updateSpeed(it) },
-                            valueRange = 0.5f..1.5f,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-                }
-            }
-        }
-
-        FloatingActionButton(
-            onClick = {
-                navController.navigate("editar")
-            },
-            modifier = Modifier
-                .padding(16.dp, 0.dp, 0.dp, 16.dp)
-                .width(200.dp * viewModel.buttonSize)
-                .align(Alignment.Bottom)
-        ) {
-
-            Row (
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(16.dp),
-                horizontalArrangement = androidx.compose.foundation.layout.Arrangement.Center
+            HorizontalDivider()
+            ScalableText(
+                text ="Interfaz",
+                textStyle = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(vertical = 8.dp),
+                accessibilityViewModel = viewModel
             )
-            {
+            SettingSlider("Tamaño de letra", viewModel.textScale, valueRange = 1.0f .. 1.4f) { viewModel.updateTextScale(it) }
+            SettingSlider("Tamaño de la interfaz", viewModel.buttonSize, valueRange = 1.0f .. 1.4f) { viewModel.buttonSize = it }
+
+            HorizontalDivider()
+            ScalableText(
+                "Texto a voz",
+                textStyle = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(vertical = 8.dp),
+                accessibilityViewModel = viewModel
+            )
+
+            SettingSlider("Tono de voz", viewModel.ttsPitch, valueRange = 0.5f..1.5f) {
+                viewModel.updatePitch(it)
+            }
+            SettingSlider("Velocidad", viewModel.ttsSpeed, valueRange = 0.5f .. 1.5f ) { viewModel.updateSpeed(it) }
+
+            HorizontalDivider()
+            ScalableText(
+                "Preguntas y categorías",
+                textStyle = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(vertical = 8.dp),
+                accessibilityViewModel = viewModel
+            )
+
+            Row(
+                modifier = Modifier
+                    .background(
+                        MaterialTheme.colorScheme.errorContainer,
+                        MaterialTheme.shapes.medium
+                    )
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .clickable { navController.navigate("editar") },
+            ) {
                 Icon(Icons.Filled.Edit, contentDescription = "Edit Questions")
                 Spacer(modifier = Modifier.width(8.dp))
-                ScalableText(text = "Editar preguntas", textStyle = MaterialTheme.typography.bodyMedium, viewModel)
+                ScalableText(
+                    text = "Editar preguntas",
+                    textStyle = MaterialTheme.typography.bodyLarge,
+                    viewModel
+                )
 
             }
         }
+
+
     }
 }
 
+
+@Composable
+fun SettingCheckbox(label: String, checked: Boolean, onCheckedChange: (Boolean) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(label)
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+    }
+}
+
+@Composable
+fun SettingSlider(
+    label: String,
+    value: Float,
+    valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
+    onValueChange: (Float) -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Text(label)
+        Slider(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier.padding(vertical = 8.dp),
+            valueRange = valueRange
+        )
+    }
+}
 
