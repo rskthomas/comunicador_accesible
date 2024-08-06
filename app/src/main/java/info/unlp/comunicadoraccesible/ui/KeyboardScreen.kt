bbadea27937dc08e7e5958cffb6051e5faba76be
Thomas Rusiecki <rusieckithomas@gmail.com>
2024-08-06 02:23:12 -0300

    the layout is now horizontal

--- app/src/main/java/info/unlp/comunicadoraccesible/ui/KeyboardScreen.kt
+++ app/src/main/java/info/unlp/comunicadoraccesible/ui/KeyboardScreen.kt
@@ -14,10 +14,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
@@ -27,7 +25,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -36,67 +33,76 @@ import info.unlp.comunicadoraccesible.composables.ScalableText
 import info.unlp.comunicadoraccesible.composables.VoiceToTextButton
 import info.unlp.comunicadoraccesible.data.AccessibilityViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun KeyboardScreen(accessibilityViewModel: AccessibilityViewModel) {
 
     var text by remember { mutableStateOf(TextFieldValue("")) }
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxSize()
+    val voiceLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { activityResult ->
+        if (activityResult.resultCode == Activity.RESULT_OK) {
+            val results =
+                activityResult.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            text = TextFieldValue(results?.get(0) ?: "")
+        }
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
     ) {
-        // 3/4 section for questions
-        Box(modifier = Modifier.weight(3f), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .weight(3f)
+                .fillMaxWidth()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
             OutlinedTextField(
-                textStyle = MaterialTheme.typography.titleLarge.copy(textAlign = TextAlign.Center),
+                textStyle = MaterialTheme.typography.titleLarge.copy(
+                    textAlign = TextAlign.Center
+                ),
                 value = text,
                 onValueChange = { text = it },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(42.dp)
-                    .background(Color.Transparent, shape = RoundedCornerShape(8.dp))
-                    .heightIn(min = 250.dp)
-                    .align(Alignment.Center),
-                shape = RoundedCornerShape(8.dp),
+                    .height(400.dp * accessibilityViewModel.textScale)
+                    .background(MaterialTheme.colorScheme.surface, shape = MaterialTheme.shapes.medium)
+                    .padding(16.dp),
+                shape = MaterialTheme.shapes.medium,
                 singleLine = false,
                 maxLines = 10,
-                supportingText = {
+                label = {
                     ScalableText(
                         text = "Escriba su consulta aquí",
-                        textStyle = MaterialTheme.typography.titleLarge,
+                        textStyle = MaterialTheme.typography.titleMedium,
                         accessibilityViewModel = accessibilityViewModel
                     )
                 }
             )
         }
 
-        val voiceLauncher = rememberLauncherForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) { activityResult ->
-            if (activityResult.resultCode == Activity.RESULT_OK) {
-                val results =
-                    activityResult.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-                text = TextFieldValue(results?.get(0) ?: "")
-            }
-        }
-
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.weight(1f)
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(16.dp)
         ) {
             ReadTextButton(
                 accessibilityViewModel = accessibilityViewModel,
                 selectedQuestion = text.text,
                 onClick = {
                     accessibilityViewModel.speakQuestion(text.text)
-                },
-                modifier = Modifier.align(Alignment.CenterHorizontally)
+                }
             )
 
-            Spacer(modifier = Modifier.height(32.dp))
-
+            Spacer(modifier = Modifier.width(100.dp))
 
             VoiceToTextButton(
                 accessibilityViewModel = accessibilityViewModel,
@@ -112,7 +118,6 @@ fun KeyboardScreen(accessibilityViewModel: AccessibilityViewModel) {
                     voiceLauncher.launch(intent)
                 }
             )
-
         }
     }
 }
